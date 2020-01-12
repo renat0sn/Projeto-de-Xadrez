132f3a1ef96d8fe482c2b81d28a0974d51dff4eb
Renato Nakano <renato.sousa.gc@hotmail.com>
2020-01-12 18:07:18 -0300

    Criacao da classe PosicaoXadrez

---
+++ Xadrez/regras/PosicaoXadrez.cs
@@ -0,0 +1,25 @@
+﻿using tabuleiro;
+
+namespace Xadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(int linha, char coluna)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + Linha + Coluna;
+        }
+    }
+}
